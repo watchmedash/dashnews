c9e3bc36161e2cbed621f3b4fe489f06899bf57e
watchmedash <138679180+watchmedash@users.noreply.github.com>
2026-03-22 05:54:36 +0400

    fix upstash env var names

--- lib/posts.ts
+++ lib/posts.ts
@@ -14,8 +14,8 @@ export interface Post {
 }
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: process.env.KV_REST_API_URL!,
+  token: process.env.KV_REST_API_TOKEN!,
 })
 
 const KEY = 'posts'
